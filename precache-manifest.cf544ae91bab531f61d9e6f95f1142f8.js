ded2b8980cf04f2a1448341ceb989ca6c98312f1
lzkids <kidsde1314@gmail.com>
2020-06-01 14:29:18 +0800

    Updates

---
+++ precache-manifest.cf544ae91bab531f61d9e6f95f1142f8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87a5861b6b7fa3489f045c0ba094830",
+    "url": "/lzkids/mysss.git/index.html"
+  },
+  {
+    "revision": "4492eef3615b779ff099",
+    "url": "/lzkids/mysss.git/static/css/2.ec297de0.chunk.css"
+  },
+  {
+    "revision": "359296d220a6f0be9f21",
+    "url": "/lzkids/mysss.git/static/css/main.3cedf80d.chunk.css"
+  },
+  {
+    "revision": "4492eef3615b779ff099",
+    "url": "/lzkids/mysss.git/static/js/2.a074d541.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/lzkids/mysss.git/static/js/2.a074d541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359296d220a6f0be9f21",
+    "url": "/lzkids/mysss.git/static/js/main.11542f62.chunk.js"
+  },
+  {
+    "revision": "91c39f579e9173aeb61c",
+    "url": "/lzkids/mysss.git/static/js/runtime-main.48fa23f2.js"
+  },
+  {
+    "revision": "3b538f76f922de087df0eb82f703119a",
+    "url": "/lzkids/mysss.git/static/media/iconfont.3b538f76.ttf"
+  },
+  {
+    "revision": "80483e3b898177297ad8b08c1e002f55",
+    "url": "/lzkids/mysss.git/static/media/iconfont.80483e3b.eot"
+  },
+  {
+    "revision": "c931e1ae5f45a521413c9905641f2071",
+    "url": "/lzkids/mysss.git/static/media/iconfont.c931e1ae.svg"
+  },
+  {
+    "revision": "f58b610e9d4e6470d644d7d195bbfda6",
+    "url": "/lzkids/mysss.git/static/media/iconfont.f58b610e.woff"
+  }
+]);
